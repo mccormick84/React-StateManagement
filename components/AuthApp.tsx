1e8da76523a04cd6475193146200f29d97f4b615
Kenny <mccormick84@gmail.com>
2022-02-04 16:30:14 +0900

    리덕스로 사용자 인증 기능 구현하기 (3)
    - AuthApp 리덕스와 연동하기

--- components/AuthApp.tsx
+++ components/AuthApp.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import {SafeAreaView, View, Text, Button, StyleSheet} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import {RootState} from '../slices';
+import {authorize, logout} from '../slices/auth';
 
 function AuthStatus() {
+  const user = useSelector((state: RootState) => state.auth.user);
+
   return (
     <View style={styles.status}>
-      <Text style={styles.text}>AuthStatus</Text>
+      <Text style={styles.text}>
+        {user ? user.displayName : '로그인이 필요합니다.'}
+      </Text>
     </View>
   );
 }
+
 function AuthButtons() {
+  const dispatch = useDispatch();
+  const onPressLogin = () => {
+    dispatch(
+      authorize({
+        id: 1,
+        username: 'john doe',
+        displayName: 'John Doe',
+      }),
+    );
+  };
+
+  const onPressLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <View>
-      <Button title={'로그인'} onPress={() => {}} />
-      <Button title={'로그아웃'} onPress={() => {}} />
+      <Button title={'로그인'} onPress={onPressLogin} />
+      <Button title={'로그아웃'} onPress={onPressLogout} />
     </View>
   );
 }
