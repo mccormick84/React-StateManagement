9ae23e132d30c436435998524b76fc0d5f85f6d3
Kenny <mccormick84@gmail.com>
2022-02-04 16:15:59 +0900

    리덕스로 사용자 인증 기능 구현하기 (2)
    - AuthApp 틀 잡아주기

--- App.tsx
+++ App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './slices';
+import AuthApp from './components/AuthApp';
 
 const store = createStore(rootReducer);
 
 export default function App() {
-  return <Provider store={store}>{/*{TODO: 컴포넌트 사용}*/}</Provider>;
+  return (
+    <Provider store={store}>
+      <AuthApp />
+    </Provider>
+  );
 }
