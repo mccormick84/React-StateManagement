67754e6f6ebdba03ca933ae186b4f4dcd38af9d7
Kenny <mccormick84@gmail.com>
2022-02-05 15:23:53 +0900

    리덕스로 CRUD 구현 (2) - TodoApp 틀 잡아주기

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './slices';
-import AuthApp from './components/AuthApp';
+import TodoApp from './components/TodoApp';
 
 const store = createStore(rootReducer);
 
 export default function App() {
   return (
     <Provider store={store}>
-      <AuthApp />
+      <TodoApp />
     </Provider>
   );
 }
